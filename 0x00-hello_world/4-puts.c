1fb92b0b16766d0b80cfb1b0634473229214391f
marceesty <marceesty@gmail.com>
2022-03-11 00:06:57 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
     * main - prints the phrase 'programing is like building a multilingial puzzle
     *Return 0 if exited poperly, non-zeo otherwise
     */
-int main()
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
