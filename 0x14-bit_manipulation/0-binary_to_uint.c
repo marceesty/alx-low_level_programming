685275ddcd58d354a75c8d4d1a6f116c6c32cd1a
marceesty <marceesty@gmail.com>
2022-04-28 23:49:01 +0100

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,13 +20,13 @@ ui = 0;
 
 for (len = 0; b[len] != '\0'; len++)
 ;
+
 for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
 if (b[len] != '0' && b[len] != '1')
 {
 return (0);
 }
-
 if (b[len] & 1)
 {
 ui += base_two;
